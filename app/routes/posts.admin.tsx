80d5323f8e8d0d788da9a947588373c59cc183ca
Toshipy <arai1209devops@gmail.com>
2025-03-01 15:45:26 +0900

    管理ページの作成

---
+++ app/routes/posts.admin.tsx
@@ -0,0 +1,35 @@
+import { json, Link, Outlet, useLoaderData } from "@remix-run/react";
+
+// eslint-disable-next-line import/no-unresolved
+import { getPosts } from "~/models/post.server";
+
+export const loader = async () => {
+	const posts = await getPosts();
+	return json({ posts });
+};
+
+export default function PostsAdmin() {
+	const { posts } = useLoaderData<typeof loader>();
+
+	return (
+		<div className="mx-auto max-w-4xl">
+			<h1 className="my-6 border-b-2 text-center text-3xl">Blog Admin</h1>
+			<div className="grid grid-cols-4 gap-6">
+				<nav>
+					<ul>
+						{posts.map((post) => (
+							<li key={post.slug}>
+								<Link to={post.slug} className="text-blue-600 underline">
+									{post.title}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<main>
+					<Outlet />
+				</main>
+			</div>
+		</div>
+	);
+}
