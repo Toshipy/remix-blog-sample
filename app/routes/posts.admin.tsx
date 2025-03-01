449f385e00e03d6ec94bf7e2d6128a2c37f026f9
Toshipy <arai1209devops@gmail.com>
2025-03-01 15:59:59 +0900

    format

--- app/routes/posts.admin.tsx
+++ app/routes/posts.admin.tsx
@@ -4,32 +4,32 @@ import { json, Link, Outlet, useLoaderData } from "@remix-run/react";
 import { getPosts } from "~/models/post.server";
 
 export const loader = async () => {
-	const posts = await getPosts();
-	return json({ posts });
+  const posts = await getPosts();
+  return json({ posts });
 };
 
 export default function PostsAdmin() {
-	const { posts } = useLoaderData<typeof loader>();
+  const { posts } = useLoaderData<typeof loader>();
 
-	return (
-		<div className="mx-auto max-w-4xl">
-			<h1 className="my-6 border-b-2 text-center text-3xl">Blog Admin</h1>
-			<div className="grid grid-cols-4 gap-6">
-				<nav>
-					<ul>
-						{posts.map((post) => (
-							<li key={post.slug}>
-								<Link to={post.slug} className="text-blue-600 underline">
-									{post.title}
-								</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-				<main className="col-span-4 md:col-span-3">
-					<Outlet />
-				</main>
-			</div>
-		</div>
-	);
+  return (
+    <div className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">Blog Admin</h1>
+      <div className="grid grid-cols-4 gap-6">
+        <nav>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.slug}>
+                <Link to={post.slug} className="text-blue-600 underline">
+                  {post.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <main className="col-span-4 md:col-span-3">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
